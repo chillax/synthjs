77cccc03206a8ae8bce715cda4b12257ca1507ba
Joonas Korhonen <jookorh@gmail.com>
2014-04-02 21:21:55 +0300

    Add support for analyser node and a drawing time domain data

--- synth.js
+++ synth.js
@@ -22,6 +22,7 @@ var Synth = (function() {
 
     Synth.prototype.route = function(nodes) {
         for(var i = 0; i < nodes.length - 1; i++) {
+			console.log(nodes[i], 'connected to', nodes[i+1]);
             nodes[i].connect(nodes[i+1]);
         }
         nodes[nodes.length - 1].connect(ctx.destination);
@@ -36,10 +37,39 @@ var Synth = (function() {
     }
 
     Synth.prototype.Gain = function(value) {
-        var gain = ctx.createGainNode();
+        var gain = ctx.createGain();
         gain.gain.value = value;
         return gain;
     };
+
+	Synth.prototype.Analyser = function(min, max) {
+		var analyser = ctx.createAnalyser();
+		analyser.minDecibels = min;
+		analyser.maxDecibels = max;
+		
+		analyser.draw = function(drawContext, canvasWidth, canvasHeight) {
+		}
+
+		return analyser;
+	}
+	
+	Synth.prototype.draw = function(analyser, drawContext, canvasWidth, canvasHeight, canvas) {
+		canvas.width = canvas.width;
+		var freqs = new Uint8Array(analyser.frequencyBinCount);
+		analyser.getByteTimeDomainData(freqs);
+
+		for (var i = 0; i < analyser.frequencyBinCount; i++) {
+			var value = freqs[i];
+			var percent = value / 256;
+			var height = canvasHeight * percent;
+			var offset = canvasHeight - height - 1;
+			var barWidth = canvasWidth/analyser.frequencyBinCount;
+			drawContext.fillStyle = 'black';
+			drawContext.fillRect(i * barWidth, offset, 1, 2);
+		}
+
+		requestAnimationFrame(this.draw.bind(this, analyser, drawContext, canvasWidth, canvasHeight, canvas));
+	}
 
     Synth.prototype.LFO = function(target) {
         // TODO
